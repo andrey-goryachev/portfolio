6f3c1f640234028209b1216bd310e5ca3ae4b684
Andrey Goryachev <goryachev.andrej@yandex.ru>
2018-03-26 10:56:37 +0300

    add flip

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,10 @@
 const mapGoogle = require('./common/map');
 const parallaxIndex = require('./common/parallaxIndex');
 const parallaxScroll = require('./common/parallaxScroll');
+const flipIndex = require('./common/flipIndex');
 
 
 parallaxIndex();
 parallaxScroll();
 mapGoogle();
+flipIndex();
---
+++ src/scripts/common/flipIndex.js
@@ -0,0 +1,25 @@
+var btnAuth = document.getElementById('btn-auth'),
+    flipContainer = document.getElementById('index-flip-container'),
+    wrapperIndex = document.getElementById('wrapper-index');
+
+var clickCount = 1;
+var clickBtn = function(event) {
+    event.stopPropagation();
+        flipContainer.style.transform = 'rotateY(180deg)';
+        btnAuth.style.left = '9999px';
+};
+
+var clickIgnore = function (event) {
+    event.stopPropagation();
+};
+
+var clickWrap = function (event) {
+    btnAuth.style.left = 'auto';
+    flipContainer.style.transform = 'rotateY(360deg)';
+};
+
+var flipper = btnAuth.addEventListener('click', clickBtn);
+var flipIgnore = wrapperIndex.addEventListener('click', clickIgnore);
+var flipBack = wrapperIndex.addEventListener('click', clickWrap);
+
+
