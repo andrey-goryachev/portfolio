a2d21935632f3f7ce38925efa401cde122b1110f
Andrey Goryachev <goryachev.andrej@yandex.ru>
2018-04-24 16:29:22 +0300

    add two works, fix slider, fix all html

--- src/scripts/common/slider.js
+++ src/scripts/common/slider.js
@@ -1,30 +1,32 @@
 import Vue from "vue";
 
-export default function slider () {
-  new Vue ({
+export default function slider() {
+  new Vue({
     el: '#works',
     data: {
       counter: 0,
       btnLeftClassName: 'works-buttons__left_',
       btnRightClassName: 'works-buttons__right_',
       activeClassImg: 'slider__item-active',
-      activeClassDesc: 'works-desc__item-active'
+      activeClassDesc: 'works-desc__item-active',
+      numWorks: 4
     },
     methods: {
-      changeBtnsImg (e) {
-        if (this.counter < 3) {
+      changeLeftBtnsImg(e) {
+        if (this.counter < this.numWorks - 1) {
           this.counter += 1;
         } else {
           this.counter = 0;
         }
+      },
+      changeRightBtnsImg(e) {
+        if (this.counter <= 0) {
+          this.counter = this.numWorks - 1;
+        } else {
+          this.counter -= 1;
+        }
+        console.log(this.counter);
       }
     }
   });
-  
-
-
-
-
-
-
 }
